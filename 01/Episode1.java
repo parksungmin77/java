1f1d010a33671352f03543f74db4c3f53c0f5da7
a2000707@naver.com <a2000707@naver.com>
2023-07-05 16:54:32 +0900

    1-1 진수

---
+++ 01/Episode1.java
@@ -0,0 +1,22 @@
+package Binary;
+
+public class Episode1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 10;
+		int bNum = 0B1010; //2진수(0B)
+		int oNum = 012; //8진수(0)
+		int hNum = 0XA; // 16진수(0X)
+		
+		
+		System.out.println(num);
+		System.out.println(bNum);
+		System.out.println(oNum);
+		System.out.println(hNum);
+		
+		
+	}
+
+}
